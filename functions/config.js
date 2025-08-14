6e79cb9d6fb88ed19763daaa2c4d338169a44849
Figgy-vt <mrfig191@gmail.com>
2025-08-14 18:51:02 -0400

    feat: use escalating multipliers for late-game items

--- functions/config.js
+++ functions/config.js
@@ -1,9 +1,15 @@
 import shopConfig from './shared/shop-config.js';
 
-export const COST_MULTIPLIER = shopConfig.costMultiplier;
+export const DEFAULT_COST_MULTIPLIER = shopConfig.costMultiplier;
 export const SHOP_ITEMS = Object.fromEntries(
   shopConfig.items.map((item) => [item.id, item.baseCost]),
 );
 export const RATES = Object.fromEntries(
   shopConfig.items.map((item) => [item.id, item.rate]),
+);
+export const COST_MULTIPLIERS = Object.fromEntries(
+  shopConfig.items.map((item) => [
+    item.id,
+    item.costMultiplier || DEFAULT_COST_MULTIPLIER,
+  ]),
 );
